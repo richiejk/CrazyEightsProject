5d53749906642f8ffc33d0c5f9927c0f6a33aff6
richiejk <richiejk@gmail.com>
2013-07-16 11:49:24 +0530

    Update SplashActivity.java
    fixed freezing of splash

--- CrazyEights/src/main/java/com/agpfd/crazyeights/activities/SplashActivity.java
+++ CrazyEights/src/main/java/com/agpfd/crazyeights/activities/SplashActivity.java
@@ -3,10 +3,12 @@ package com.agpfd.crazyeights.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
 
 import com.agpfd.crazyeights.R;
+
 
 import views.TitleView;
 
@@ -18,13 +20,21 @@ public class SplashActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.splash);
-        try {
+        new android.os.Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,CrazyEightsActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
+/*        try {
             Thread.sleep(7000);
             Intent intent=new Intent(SplashActivity.this,CrazyEightsActivity.class);
             startActivity(intent);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }
+        }*/
 
     }
 
